0e24684106670194a35b6a9adcf5b6ab46355748
unknown <sangyeonlee@joycity.com>
2022-11-24 19:09:00 +0900

    Modified pch.h

--- GameServer/SocketServer/SocketServer/pch.h
+++ GameServer/SocketServer/SocketServer/pch.h
@@ -15,23 +15,6 @@
 #include <ws2tcpip.h>
 #include <windows.h>
 
-//#pragma comment(lib, "ws2_32.lib")
-//
-//#define _SILENCE_CXX20_CISO646_REMOVED_WARNING
-//#define _SILENCE_ALL_CXX20_DEPRECATION_WARNINGS
-//
-//#ifdef _DEBUG
-//#pragma comment(lib, "Protobuf\\_Debug\\libprotobufd.lib")
-//#pragma comment(lib, "ServerCoreLibrary\\Debug\\ServerCoreLibrary.lib")
-//#pragma comment(lib, "SharedProtobuf\\Debug\\SharedProtobuf.lib")
-//#pragma comment(lib, "PhysicsEngine\\Debug\\PhysicsEngine.lib")
-//#else
-//#pragma comment(lib, "Protobuf\\_Release\\libprotobuf.lib")
-//#pragma comment(lib, "ServerCoreLibrary\\Release\\ServerCoreLibrary.lib")
-//#pragma comment(lib, "ServerCoreLibrary\\Release\\SharedProtobuf.lib")
-//#pragma comment(lib, "PhysicsEngine\\Release\\PhysicsEngine.lib")
-//#endif
-
 #define STR(arg) string(arg)
 
 using std::function;
